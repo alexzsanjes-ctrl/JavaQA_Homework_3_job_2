4206b07d98c47aed5666942774cad7224b3c9fb3
Александр Саломанин <alexzsanjes@gmail.com>
2025-12-22 23:41:04 +0700

    type changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(int weight, double height) {
+    public int calculate(int weight, double height) {
         double bmi;
         bmi = weight / (height * height);
         return (int) bmi;
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import jdk.swing.interop.LightweightContentWrapper;
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate(82, 1.79);
+        int bmi = service.calculate(98, 1.87);
         System.out.println("Индекс массы тела равен: " + (bmi));
     }
 }
